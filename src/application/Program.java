fbc4a818a40fcaf125b68ec517ee35e7c948effd
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-10-24 14:35:11 -0300

    criando logica de CHECKMATE

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				
 				UI.clearScreen();
@@ -47,5 +47,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
