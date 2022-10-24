a94e4e2746e5497894e4a6cc476b59cf511a22c7
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-10-24 15:44:19 -0300

    implementando contagem de movimentos e peças peão

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,7 +55,6 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 		if (!chessMatch.getCheckMate()) {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 			if (chessMatch.getCheck()) {
